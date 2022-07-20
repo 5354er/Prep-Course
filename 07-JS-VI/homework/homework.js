c186512599ecba3a123a67e060159f917fadd69c
Efren R <efren6431@gmail.com>
2022-07-20 18:20:02 -0500

    JS 07 homework complete

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,43 +4,77 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  capitalize = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  return capitalize;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1 + n2);
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  //Tu código:
+  //Tu código: 
+  var sumarArray = numeros.reduce(function(accumulador, elemento) {
+    return accumulador + elemento;
+  },0);
+    cb(sumarArray);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  //for(var i = 0; i < array.length; i++) {
+    //cb(array[i]);
+  //}
+  array.forEach(function(array) {
+      cb(array)
+  });
 }
-
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray = array.map(function (elemento) {
+    
+      return cb(elemento);
+    
+  });
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  //syntax: var newArray = arr.filter(callback(element[, index[, array]])
+//  ↑ [, thisArg]) ↑
+    //var words = ["hi", "hello", "hey", "apple", "watermelon",
+        //   ↑ "lemon", "javascript"];  ↑
+    //apply arrow function
+    //var result = words.filter(word => word.length > 5);
+       //   ↑ console.log(result);  ↑
+    
+    var nuevoArray = [];
+  for(let i = 0; i<array.length; i++) {
+    if(array[i][0] === "a") {
+      nuevoArray.push(array[i])
+    }
+  }
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
